e2f8d819bb437e65b093b269d41a3e96866b76bc
Odin Grav <odin@grav.name>
2026-03-20 13:56:46 +0100

    updates test

--- src/test/java/edu/ntnu/idatt2003/odingr/models/DeckOfCardsTest.java
+++ src/test/java/edu/ntnu/idatt2003/odingr/models/DeckOfCardsTest.java
@@ -1,4 +1,4 @@
-package edu.ntnu.idatt2003.odingr;
+package edu.ntnu.idatt2003.odingr.models;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
